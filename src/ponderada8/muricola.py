39b8bf1c52fce3802455ae69d676d73aaac93fef
gabrielpascoli <99223332+gabrielpascoli@users.noreply.github.com>
2023-12-21 22:22:12 -0300

    HAPPY

--- src/ponderada8/muricola.py
+++ src/ponderada8/muricola.py
@@ -5,13 +5,19 @@ import sys
 import os
 
 class MURICOLA:
-    def __init__(self):
+    def init(self):
 
-        self.audio_file = sys.argv[1]
         self.murilo = MURILO()
         self.potara = POTARA()
         self.nicola = NICOLA()
         self.audio = "speech.mp3"
+        self.audio_file = self.murilo.convert(sys.argv[1])
+
+    def supremo(self):
+        text = self.murilo.stt(self.audio_file)
+        text = self.potara.translate(text)
+        self.audio = self.nicola.tts(text)
+        self.nicola.play_audio(self.audio)
         
         if len(sys.argv) != 2 or not os.path.exists(self.audio_file) or not self.audio_file.endswith(".wav"):
             print("ERROR: Invalid arguments.")
